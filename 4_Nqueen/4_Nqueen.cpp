06771f92171d716acc51fde7a2cf2dccbea917de
Anish Wakchoure <149792581+AnishWakchoure@users.noreply.github.com>
2026-05-11 00:46:06 +0000

    rest codes

---
+++ 4_Nqueen/4_Nqueen.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isSafe(int **arr, int x, int y, int n){
+    for(int row=0;row<x;row++){
+        if(arr[row][y]==1){
+            return false;
+        }
+    }
+
+    int row =x;
+    int col =y;
+    while(row>=0 && col>=0){
+        if(arr[row][col]==1){
+            return false;
+        }
+        row--;
+        col--;
+    }
+
+    row =x;
+    col =y;
+    while(row>=0 && col<n){
+        if(arr[row][col]==1){
+            return false;
+        }
+        row--;
+        col++;
+    }
+
+    return true;
+}
+
+bool nQueen(int** arr, int x, int n){
+
+    if(x>=n){   
+        return true;
+    }
+
+    for(int col=0;col<n;col++){
+        if(isSafe(arr,x,col,n)){
+            arr[x][col]=1;
+            if(nQueen(arr,x+1,n)){ 
+                return true;
+            }
+            arr[x][col]=0;
+        }
+    }
+    return false;
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    
+    int **arr = new int*[n];    
+    for(int i=0;i<n;i++){
+        arr[i] = new int[n];
+        for(int j=0;j<n;j++){
+            arr[i][j]=0;
+        }
+    }
+
+    if(nQueen(arr,0,n)){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cout << arr[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
+/*
+input = 4
+The program will attempt to solve the 4-Queens problem.
+Output:
+0 1 0 0 
+0 0 0 1 
+1 0 0 0 
+0 0 1 0 
+This means:
+•	A queen is placed at (0, 1).
+•	Another queen is placed at (1, 3).
+•	Another queen is placed at (2, 0).
+•	Another queen is placed at (3, 2).
+________________________________________
+
+run cmd - 
+g++ 4_Nqueen.cpp -o nqueen
+./nqueen
+*/
